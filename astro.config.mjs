21ae812f7001bb07635d78397aba60108e36a787
oshimomoto1111 <oshimomoto1111@users.noreply.github.com>
2026-08-24 16:17:06 -0600

    Configure custom domain dukecityfencing.net in Astro and CNAME

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://oshimomoto1111.github.io',
-  base: '/dukecityfencingclub',
+  site: 'https://dukecityfencing.net',
+  base: '/',
 });
